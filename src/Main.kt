7e58d11da5a749b44454a31b9f2ba0c9ebe247be
P466392 <P466392@tafe.wa.edu.au>
2025-08-22 09:41:35 +0800

    Start work on clock

---
+++ src/Main.kt
@@ -0,0 +1,11 @@
+/*
+    TODO: Class variables to store: hours (0-23), minutes (0-59), seconds (0-59).
+    TODO: Primary constructor (No parameters): Initializes the hours, minutes, and seconds to 0.
+    TODO: Secondary constructor: Initializes the hours, minutes, and seconds using parameters entered by user.
+    TODO: Method1: Reset the hours, minutes and seconds back to 0.
+    TODO: Method2: Advance the clock by 1 second.
+    */
+
+fun main() {
+
+}
