fec089efe5415f646cb1bfddfac018a580af51c3
P466392 <P466392@tafe.wa.edu.au>
2025-09-05 11:54:33 +0800

    Added the clock class

---
+++ src/Clock.kt
@@ -0,0 +1,12 @@
+class Clock {
+    private var hour = 0
+    private var minute = 0
+    private var second = 0
+
+    fun defaultClock() {
+        hour = 0
+        minute = 0
+        second = 0
+    }
+
+}
